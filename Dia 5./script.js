ed4dba4ba56d793be8bcb5d4511e254ca2b03eb7
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-03-29 13:48:06 -0300

    Update script.js

--- Dia 5./script.js
+++ Dia 5./script.js
@@ -1,3 +1,9 @@
+/*
+É necessário usar o link da imagem
+Para testar o código, pode-se usar o determinado link como exemplo: 
+"https://m.media-amazon.com/images/M/MV5BMTYzMDM4NzkxOV5BMl5BanBnXkFtZTgwNzM1Mzg2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"
+Esse é o link do cartaz do filme Toy Story 4
+*/
 function adicionarFilme() {
     var campoFilmeFavorito = document.querySelector("#filme")
     var FilmeFavorito = campoFilmeFavorito.value
